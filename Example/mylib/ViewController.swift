0451b11c4b59666c8c86b9d85193b2370486d09d
Tan Cher Shen <tan_cher_shen@tech.gov.sg>
2022-12-23 14:28:58 +0800

    my commit

--- Example/mylib/ViewController.swift
+++ Example/mylib/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import mylib
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(ReplaceMe.hello())
     }
 
     override func didReceiveMemoryWarning() {
---
+++ mylib/Classes/ReplaceMe.swift
@@ -0,0 +1,5 @@
+public class ReplaceMe {
+    public static func hello() -> String {
+        return "Hello!"
+    }
+}
