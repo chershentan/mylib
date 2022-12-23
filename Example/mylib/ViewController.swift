1c110739047c9d72267765c0d4eb689d91b6cd61
Tan Cher Shen <tan_cher_shen@tech.gov.sg>
2022-12-23 14:17:32 +0800

    Initial commit

---
+++ Example/mylib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  mylib
+//
+//  Created by Tan Cher Shen on 12/23/2022.
+//  Copyright (c) 2022 Tan Cher Shen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
